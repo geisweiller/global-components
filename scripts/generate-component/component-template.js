d67e33b406336790fc33d02270b2f0d21fd04a8e
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2024-01-11 10:59:44 -0300

    chore(scripts): generate component

---
+++ scripts/generate-component/component-template.js
@@ -0,0 +1,76 @@
+// component.tsx
+export function component(name, fileName) {
+  return `import React from 'react';
+import styles from './${fileName}.module.css';
+
+interface ${name}Props {
+  /**
+   * This explains foo.
+   */
+  foo: boolean;
+  /**
+   * This explains bar.
+   */
+  bar: string;
+  /**
+   * This explains baz.
+   */
+  baz: string;
+}
+
+const ${name} = ({ foo, bar, baz }: ${name}Props) => (
+  <div className={styles.wrapper}>
+    <p>Hello 👋, I am a ${name} component.</p>
+    <div>{foo ? bar : baz}</div>
+  </div>
+);
+
+export default ${name};
+`;
+}
+
+// component.stories.jsx
+export function story(name, fileName) {
+  return `import React from 'react';
+import ${name} from './${fileName}';
+
+export default {
+  title: 'Components/${name}',
+  component: ${name},
+};
+
+const Template = (args) => <${name} {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  foo: true,
+  bar: 'Yes Foo',
+  baz: 'No Foo',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  foo: false,
+  bar: 'Yes Foo',
+  baz: 'No Foo',
+};
+`;
+}
+
+// component.test.tsx
+export function test(fileName) {
+  return `import React from 'react';
+import { render } from '@testing-library/react';
+import { Default, Secondary } from './${fileName}.stories.js';
+
+test('renders Bar when foo is true', () => {
+  const { getByText } = render(<Default {...Default.args} />);
+  expect(getByText(Default.args.bar)).toBeInTheDocument();
+});
+
+test('renders Baz when foo is false', () => {
+  const { getByText } = render(<Secondary {...Secondary.args} />);
+  expect(getByText(Secondary.args.baz)).toBeInTheDocument();
+});
+`;
+}
