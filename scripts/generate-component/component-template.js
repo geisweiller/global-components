a68b9a7f688ce3ee0c4097210e9bb55d70756125
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2024-01-15 12:12:29 -0300

    fix(scripts): typo

--- scripts/generate-component/component-template.js
+++ scripts/generate-component/component-template.js
@@ -37,7 +37,7 @@ import ${name} from "./${fileName}";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Component/${name}",
+  title: "Components/${name}",
   component: ${name},
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
