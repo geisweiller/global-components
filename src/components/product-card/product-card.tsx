610c33698026e0aad02824656a3a671250104751
IURI <123806396+ithauront@users.noreply.github.com>
2024-03-01 20:09:42 +0100

    addedCounter(componentsProductCard):unsing component counter on component productCard

--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -1,4 +1,6 @@
-interface CardProps {
+import {Counter} from '../counter/counter'
+
+interface ProductCardProps {
   /**
    *(optional)  The adress for the image of the product. 
    */
@@ -14,9 +16,9 @@ interface CardProps {
 }
 
 // This is a Card to display products. You can rearange the html tags to have it on different formats.
-
-
-export const card = ({ image, title, description }: CardProps) => {
+// This component will use tailwind css for styles to give a basic default of styles for it.
+// This component uses other components from this repository (counter) for it to work you will have to have a counter (ours or an of your own) on your project. If you don't need a counter on your card you can just remove it from the html tags and imports.
+export const ProductCard = ({ image, title, description }: ProductCardProps) => {
   const imageTitle = `image of ${title}`
   return(
     
@@ -24,6 +26,7 @@ export const card = ({ image, title, description }: CardProps) => {
     <img src={image} alt={imageTitle} />
     <h3>{title}</h3>
     <p>{description}</p>
+    <Counter />
   </div>
 )
 };
